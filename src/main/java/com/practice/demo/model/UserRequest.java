bdeaf4cbdf37d4190dee10584c9f136cc8867351
Mayur Mulani <mayur.mulani@cognizant.com>
2023-03-01 16:39:06 +1100

    CRUD OPERATION UPDATED

---
+++ src/main/java/com/practice/demo/model/UserRequest.java
@@ -0,0 +1,57 @@
+package com.practice.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRequest {
+
+	@JsonProperty("userName")
+	private String userName;
+
+	@JsonProperty("firstName")
+	private String firstName;
+
+	@JsonProperty("lastName")
+	private String lastName;
+
+	public UserRequest() {
+	}
+
+	public UserRequest(String userName, String firstName, String lastName) {
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
